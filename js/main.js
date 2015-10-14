89d23ba0dee873e67c1d9f31d77c50e6d5c2eb7d
IgorLav <i.lavskij@gmai.com>
2015-10-14 18:38:28 +0300

    lavs

--- js/main.js
+++ js/main.js
@@ -158,68 +158,24 @@ $(document).ready( function(){
 
  
 
- /*
+ 
 
     //Customize select dropdowns
-
-    $('select').each(function(){
-        var $this = $(this), numberOfOptions = $(this).children('option').length;
-      
-       // $this.addClass('select-hidden'); 
-        $this.wrap('<div class="select"></div>');
-        $this.after('<div class="select-styled"></div>');
-
-        var $styledSelect = $this.next('div.select-styled');
-        $styledSelect.text($this.children('option').eq(0).text());
-      
-        var $list = $('<ul />', {
-            'class': 'select-options'
-        }).insertAfter($styledSelect);
-      
-        for (var i = 0; i < numberOfOptions; i++) {
-            $('<li />', {
-                text: $this.children('option').eq(i).text(),
-                rel: $this.children('option').eq(i).val()
-            }).appendTo($list);
-        }
-      
-        var $listItems = $list.children('li');
-      
-        $styledSelect.click(function(e) {
-            e.stopPropagation();
-            $('div.select-styled.active').each(function(){
-                $(this).removeClass('active').next('ul.select-options').hide();
-
-            });
-            $(this).toggleClass('active').next('ul.select-options').toggle();
+  $(function() {
+    $("select.custom").each(function() {                    
+        var sb = new SelectBox({
+            selectbox: $(this),
+            customScrollbar: true,
+            height: 350,
         });
-      
-        $listItems.click(function(e) {
-            e.stopPropagation();
-            $styledSelect.text($(this).text()).removeClass('active');
-            $this.val($(this).attr('rel'));
-            $list.hide();
-            //console.log($this.val());
-        });
-      
-        $(document).click(function() {
-            $styledSelect.removeClass('active');
-            $list.hide();
-        });
-
     });
-    $(".sidebar-filter aside").affix({
-        offset: { 
-            top: 105 
-        }
-    });
-
+    
 });
- */
+
 
 
 //bootstrap verical slider
-   // $('#carousel-vertical').carousel();
+    $('#carousel-vertical').carousel();
 /*
     var nextHeading = $('.item.active').next().data('heading');
     var prevHeading = $('.item.active').prev().data('heading');

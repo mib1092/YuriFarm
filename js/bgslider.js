925bf0344cb231f82880f41b1b755d83862d143a
IgorLav <i.lavskij@gmai.com>
2015-10-22 02:34:10 +0300

    lavs

--- js/bgslider.js
+++ js/bgslider.js
@@ -1,30 +1,30 @@
 $(function(){
-		$('#maximage').maximage({
-			cycleOptions: {
-				fx: 'fade',
-				speed: 1000,  
-				timeout: 0,
-				fillElement: '.slider-wrap',
-				pager: ".slider-pagination ul",
-				pagerAnchorBuilder: function(slide) {
-	   				 return '<li><a href="#" rel="nofollow"></a></li>';
-				},
-				pause: 1
-				
+	$('#maximage').maximage({
+		cycleOptions: {
+			fx: 'fade',
+			speed: 1000,  
+			timeout: 0,
+			fillElement: '.slider-wrap',
+			pager: ".slider-pagination ul",
+			pagerAnchorBuilder: function(slide) {
+   				 return '<li><a href="#" rel="nofollow"></a></li>';
 			},
-			onFirstImageLoaded: function(){
-				jQuery('#cycle-loader').hide();
-				jQuery('#maximage').fadeIn('fast');
-			},
-			debug:true
-		});
+			pause: 1
 			
-		// Helper function to Fill and Center the HTML5 Video
-		jQuery('.html5video').maximage('maxcover');
-			
-		// To show it is dynamic html text
-		jQuery('.in-slide-content').delay(1200).fadeIn();
+		},
+		onFirstImageLoaded: function(){
+			jQuery('#cycle-loader').hide();
+			jQuery('#maximage').fadeIn('fast');
+		},
+		debug:true
 	});
+		
+	// Helper function to Fill and Center the HTML5 Video
+	jQuery('.html5video').maximage('maxcover');
+		
+	// To show it is dynamic html text
+	jQuery('.in-slide-content').delay(1200).fadeIn();
+})
 
 	$(function pagin(){
 	var bgSlide = $('.slider-pagination .activeSlide').index();
@@ -39,12 +39,8 @@ $(function(){
 
     
     //set height for slider-wrap
-    function setHeiHeight() {
-        var newScreenSize = $(window).height();
-        $('.slider-wrap').css({'top': newScreenSize});
-        console.log($(window).height());
-    }
 
-    setHeiHeight();
-    $(window).resize( setHeiHeight );
+
+     
+
 });

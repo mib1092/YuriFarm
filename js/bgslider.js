da2f158840f1808ce7c96c9b30ed1ae170792794
IgorLav <i.lavskij@gmai.com>
2015-10-22 01:39:41 +0300

    slider index

--- js/bgslider.js
+++ js/bgslider.js
@@ -1,51 +1,48 @@
 $(function(){
-	$('#maximage').maximage({
-		cycleOptions: {
-			fx: 'fade',
-			speed: 1000, // Has to match the speed for CSS transitions in jQuery.maximage.css (lines 30 - 33)
-			timeout: 0,
-			pager: ".slider-pagination ul",
-			pagerAnchorBuilder: function(slide) {
-   				 return '<li><a href="#" rel="nofollow"></a></li>';
+		$('#maximage').maximage({
+			cycleOptions: {
+				fx: 'fade',
+				speed: 1000,  
+				timeout: 0,
+				fillElement: '.slider-wrap',
+				pager: ".slider-pagination ul",
+				pagerAnchorBuilder: function(slide) {
+	   				 return '<li><a href="#" rel="nofollow"></a></li>';
+				},
+				pause: 1
+				
 			},
-			pause: 0
-			 
-		},
-
-		onFirstImageLoaded: function(){
-			jQuery('#cycle-loader').hide();
-			jQuery('#maximage').fadeIn('fast');
-		},
-		debug:true
-	});
-		$(function pagin(){
-			var bgSlide = $('.slider-pagination .activeSlide').index();
-				 setTimeout(pagin, 30);
-	   		 $('.slider-pagination ul li').click(function(){
-		    	for(i>0; i<bgSlide; i++){
-		    		$('.slider-pagination ul li a').addClass('viewed');
-		    	}
-		    });
+			onFirstImageLoaded: function(){
+				jQuery('#cycle-loader').hide();
+				jQuery('#maximage').fadeIn('fast');
+			},
+			debug:true
 		});
- 	
+			
+		// Helper function to Fill and Center the HTML5 Video
+		jQuery('.html5video').maximage('maxcover');
+			
+		// To show it is dynamic html text
+		jQuery('.in-slide-content').delay(1200).fadeIn();
+	});
 
-	// Helper function to Fill and Center the HTML5 Video
-	$('.html5video').maximage('maxcover');
-		
-	// To show it is dynamic html text
-	$('.in-slide-content').delay(1200).fadeIn();
+	$(function pagin(){
+	var bgSlide = $('.slider-pagination .activeSlide').index();
+		 setTimeout(pagin, 30);
+		 $('.slider-pagination ul li').click(function(){
+		for(i>0; i<bgSlide; i++){
+			$('.slider-pagination ul li a').addClass('viewed');
+		}
+	});
+ 
 
 
     
     //set height for slider-wrap
     function setHeiHeight() {
         var newScreenSize = $(window).height();
-        $('.slider-wrap').css({
-            height: newScreenSize + 'px'
-        });
-        $('.slider-wrap video').css({
-            height: newScreenSize + 'px'
-        });
+        $('.slider-wrap').css({'top': newScreenSize});
+        console.log($(window).height());
     }
 
     setHeiHeight();

71933060d71ccd8e97dd6299717dae13a7d9a3fc
IgorLav <i.lavskij@gmai.com>
2015-10-27 17:53:39 +0200

    li

--- js/main.js
+++ js/main.js
@@ -373,24 +373,5 @@ $(document).ready(function(){
         map.height(diff);
     }
 
-    var owl = $("#otherProductsCarousel");
-        owl.owlCarousel({
-          items : 4, //10 items above 1000px browser width
-          itemsTablet: [768, 2], //2 items between 600 and 0
-          pagination: false,
-          autoPlay : true,
-          stopOnHover : true,
-          navigationText: false,
-          navigation : true
-      });
-     
-      // Custom Navigation Events
-      $(".next").click(function(){
-        owl.trigger('owl.next');
-      })
-      $(".prev").click(function(){
-        owl.trigger('owl.prev');
-      })
+
 });
-
-
---
+++ js/productSlider.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	var owl = $("#otherProductsCarousel");
+        owl.owlCarousel({
+          items : 4, //10 items above 1000px browser width
+          itemsTablet: [768, 2], //2 items between 600 and 0
+          pagination: false,
+          autoPlay : true,
+          stopOnHover : true,
+          navigationText: false,
+          navigation : true
+      });
+     
+      // Custom Navigation Events
+      $(".next").click(function(){
+        owl.trigger('owl.next');
+      })
+      $(".prev").click(function(){
+        owl.trigger('owl.prev');
+      })
+});

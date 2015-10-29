de3bbe1fa8acc04c6f67c9bbe79fa45923356a18
IgorLav <i.lavskij@gmai.com>
2015-10-29 22:13:43 +0200

    li

--- js/main.js
+++ js/main.js
@@ -454,4 +454,24 @@ $(document).ready(function(){
     });
 
    */
+    var map;
+    var ajaxRequest;
+    var plotlist;
+    var plotlayers=[];
+
+
+    function initmap() {
+        // set up the map
+        map = new L.Map('map');
+
+        // create the tile layer with correct attribution
+        var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        var osm = new L.TileLayer(osmUrl, {minZoom: 15, maxZoom: 19});       
+
+        // start the map in 
+        map.setView(new L.LatLng(50.42016,30.50178), 18);
+        map.addLayer(osm);
+    }
+
+    initmap();
 });

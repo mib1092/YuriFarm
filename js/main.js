a753180b11e6c14a3a914fa229f325bf5285d3f2
IgorLav <i.lavskij@gmai.com>
2015-11-24 23:08:42 +0200

    li

--- js/main.js
+++ js/main.js
@@ -137,21 +137,7 @@ $(window).ready( function(){
             $('.sidemenu-wrap').removeClass('open-menu');
             $('body').removeClass('fixed');
         }); 
-
-
-        //swipe menu left for close
-        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && $(window).width() < 650) {
-            $(".sidemenu-wrap").swipe({
-            swipeStatus: function(event, phase, direction, distance, duration, fingers){
-              if (phase == "move" && direction == "left"){
-                $(".sidemenu-wrap").removeClass("open-menu");
-                $('body').removeClass('fixed');
-                return false;
-              }
-            }, allowPageScroll:"vertical"
-
-          });  
-         }
+ 
     };
 
     sidemenuToggle();

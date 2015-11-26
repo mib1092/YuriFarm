2bf03e4e30f09eab010e6466724acaa264118ec6
IgorLav <i.lavskij@gmai.com>
2015-11-26 19:25:27 +0200

    ;

--- js/main.js
+++ js/main.js
@@ -497,9 +497,9 @@ $(function() {
         $.fn.animated = function(inEffect, outEffect) {
                 $(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
                         if (dir === "down") {
-                                $(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+                                $(this).addClass(inEffect).css("opacity", "1");
                         } else {
-                                $(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+                                $(this).addClass(outEffect).css("opacity", "1");
                         };
                 }, {
                         offset: "80%"
